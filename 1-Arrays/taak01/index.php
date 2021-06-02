17ebeea6778b58f2f88930a176fa1999efc8d7ef
ImranKaouch <2122928@talnet.nl>
2021-06-02 11:48:35 +0200

    taak

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php
+$studenten = array("Piet", "Muhammad", "Els"); 
+$achternaam['Piet'] = 'Jan';
+$achternaam['Muhammad'] = 'Ali';
+$achternaam['Els'] = 'Elizabeth';
+$leeftijd['Piet'] = 17;
+$leefijd['Muhammad'] = 17;
+$leeftijd['Els'] = 17;
+echo  "Gegevens Muhammad: Voornaam = " . $studenten [1] . ". Achternaam = " . $achternaam['Muhammad'] . ". Leeftijd = " . $leeftijd['Muhammad'] . ". Klas = ";
+?>
+
+</body>
+</html>
