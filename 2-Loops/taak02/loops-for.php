82a35529923ef5aa0920004d697135235049eace
ImranKaouch <2122928@talnet.nl>
2021-06-08 14:22:51 +0200

    taakk02

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php
+echo "<ul>";
+for ($x = 0; $x < 18; $x++) {  
+echo  "<li>Ik ben $x jaar, dus ik mag nog niet stemmen</li>";
+}
+echo "<li>Ik ben 18 jaar oud dus ik heb stemrecht!</li>";
+echo "</ul>"
+
+
+
+
+
+?>    
+</body>
+</html>
