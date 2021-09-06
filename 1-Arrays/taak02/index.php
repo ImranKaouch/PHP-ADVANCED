d47cfddd060d504ea53c6d8867f10a54c8516e9d
ImranKaouch <2122928@talnet.nl>
2021-09-06 14:30:30 +0200

    taak02

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -8,7 +8,7 @@ $student ["achternaam"] = "Kaouch";
 $student ["klas"] = "OA";
 $student ["leeftijd"] = "17";
 
-echo "Mijn naam is ". $student["voornaam"] . $student["achternaam"] . ". Ik zit in klas ". $student["klas"] . "en ik ben ". $student["leeftijd"] . " jaar oud."
+echo "Mijn naam is ". $student["voornaam"] ." " . $student["achternaam"] . ". Ik zit in klas ". $student["klas"] . " en ik ben ". $student["leeftijd"] . " jaar oud."
 
 
 
