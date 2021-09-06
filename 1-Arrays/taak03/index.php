a4dac449347fd0dd7a8cfe645247b92856b93ba9
ImranKaouch <2122928@talnet.nl>
2021-09-06 15:11:42 +0200

    taak03

---
+++ 1-Arrays/taak03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<body>
+    <?php
+
+$persoon ["voornaam"] = "Willem Alexander";
+$persoon ["achternaam"] = "van Oranje";
+$persoon ["leeftijd"] = "53";
+$persoon ["opleiding"] = "Software Ontwikkeling";
+
+echo  $persoon["voornaam"] ." " . $persoon["achternaam"] . " is " . $persoon["leeftijd"] .  " jaar oud en is student aan het ROC van Amsterdam aan de opleiding " . $persoon["opleiding"] . " ";
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
