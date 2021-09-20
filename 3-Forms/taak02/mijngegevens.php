918eefe77f78644588346d15abc81d5ce012dd2e
ImranKaouch <2122928@talnet.nl>
2021-09-20 13:37:41 +0200

    taak03

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -7,30 +7,30 @@ $colour = $_POST["colour"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mijn gegevens</title>
-        <style>
+    <title>Mijn evens</title>
+    <style>
             body{
                 background-color: <?php echo $colour;?>
+
             }
-            label{
-                width:100px;
-                height:100px;
-            }
+            
+
+            
+        
         </style>
 </head>
 <body>
     
-    <?php echo $_POST["voornaam"]; ?><br>
+    <?php echo $_POST["voornaam"] . " ".  $_POST["achternaam"] ; ?><br>
       <?php echo $_POST["geslacht"];?><br>
-      <?php echo $_POST["achternaam"]; ?><br>
-      <?php echo $_POST["klas"]; ?><br>
-      <?php echo $_POST["leeftijd"]; ?><br>
+      <?php echo "Klas:" . $_POST["klas"]; ?><br>
+      <?php echo $_POST["leeftijd"]. " Jaar"; ?><br>
       <?php echo $_POST["adres"]; ?><br>
-      <?php echo $_POST["plaatsnaam"];?><br>
+      <?php echo $_POST["woonplaats"];?><br>
     
      
 
       <!-- I AM TRYING TO MAKE THE COLOUR APEAR ON THIS SCREEN WITH THE GIVEN USER INPUT.-->
-      <?php echo $colour; ?>
+      
 </body>
 </html>
